2974a13622e023ffbea526876b558adada740197
ikoote1 <ikootepreim@gmail.com>
2023-05-02 15:54:59 +0300

    adding queries

--- queries.sql
+++ queries.sql
@@ -3,3 +3,5 @@
 SELECT*FROM animals WHERE name LIKE '%mon%';
 SELECT*FROM animals WHERE neutered=TRUE AND escape_attempts<3;
 SELECT name,date_of_birth  FROM animals WHERE name='Agumon' OR name='Pikachu';
+SELECT name,escape_attempts FROM animals WHERE weight_kg>10.5;
+SELECT*FROM animals WHERE neutered=TRUE;
