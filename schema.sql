1eef35b5bf75b72a397d36c4d0f6e37464963e5e
ikoote1 <ikootepreim@gmail.com>
2023-05-02 15:01:10 +0300

    adding queries

--- schema.sql
+++ schema.sql
@@ -1,5 +1,5 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+create database vet_clinic;
+
+create table animals (id integer, name varchar, date_of_birth date, escape_attempts integer, neutered boolean, weight_kg decimal);
