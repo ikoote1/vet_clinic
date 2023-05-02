1c6d215a5a4ffa5b4f2a36a7206cbffe328443d0
ikoote1 <ikootepreim@gmail.com>
2023-05-02 17:46:06 +0300

    adding queries

--- data.sql
+++ data.sql
@@ -1,3 +1,3 @@
-/* Populate database with sample data. */
+/* Populate database with sample data.a*/
 
 INSERT INTO animals VALUES (1,'Agumon','2020/02/03',0,TRUE,10.23),(2,'Gabumon','2018/11/15',2,TRUE,8),(3,'Pikachu','2021/01/07',1,FALSE,15.04),(4,'Devimon','2017/05/12',5,TRUE,11);
