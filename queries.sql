aafdc4abab70ca84ebc880baa273e9786205860a
ikoote1 <ikootepreim@gmail.com>
2023-05-03 14:31:45 +0300

    adding queries

--- queries.sql
+++ queries.sql
@@ -6,3 +6,10 @@ SELECT name,escape_attempts FROM animals WHERE weight_kg>10.5;
 SELECT*FROM animals WHERE neutered=TRUE;
 SELECT*FROM animals WHERE name NOT IN ('Gabumon');
 SELECT* FROM animals WHERE (weight_kg>10.4 OR weight_kg=10.4) AND (weight_kg=17.3 OR weight_kg<17.3);
+
+-- Transaction and updated
+UPDATE animals SET species = 'unspecified';
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon%';
+UPDATE animals SET species = 'pokemon' WHERE species NOT IN ('digimon');
+ DELETE FROM animals;
+ DELETE FROM animals WHERE date_of_birth > '2022-01-01';
--- schema.sql
+++ schema.sql
@@ -3,3 +3,4 @@
 create database vet_clinic;
 create table animals (id integer NOT NULL, name varchar NOT NULL, date_of_birth date NOT NULL, escape_attempts integer NOT NULL, neutered boolean NOT NULL, weight_kg decimal NOT NULL);
 ALTER TABLE animals ADD COLUMN species varchar;
+
