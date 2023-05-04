69aa3a393817b10a0c2720dbc5f04a6aed67c0b1
ikoote1 <ikootepreim@gmail.com>
2023-05-04 11:49:56 +0300

    adding queries

--- queries.sql
+++ queries.sql
@@ -27,4 +27,6 @@ SELECT MAX(weight_kg),MIN(weight_kg) FROM animals;
 SELECT AVG(escape_attempts) FROM animals WHERE date_of_birth between '1990-12-31' AND '2000-01-01';
 
 -- foreign key and JOIN
+INSERT INTO owners (name,age) VALUES ('Sam Smith',34),('Jennifer Orwell',19),('Bob',45),('Melody Pond',77),('Dean Winchester',14),('Jodie Whittaker',38);
+ INSERT INTO species (name) VALUES ('Pokemon'),('Digimon');
 
