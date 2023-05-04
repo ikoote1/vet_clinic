b3805f641dd558b079faccf4aa89e3e62ce96436
ikoote1 <ikootepreim@gmail.com>
2023-05-04 12:55:46 +0300

    adding queries

--- queries.sql
+++ queries.sql
@@ -27,4 +27,4 @@ SELECT MAX(weight_kg),MIN(weight_kg) FROM animals;
 SELECT AVG(escape_attempts) FROM animals WHERE date_of_birth between '1990-12-31' AND '2000-01-01';
 
 -- foreign key and JOIN
-UPDATE animals SET species_id = 2 WHERE name LIKE '%mon%';
+
