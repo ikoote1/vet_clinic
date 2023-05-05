fa62e9201e61063b2492091ebb4c7b978c77182b
ikoote1 <ikootepreim@gmail.com>
2023-05-05 09:48:39 +0300

    adding queries

--- schema.sql
+++ schema.sql
@@ -12,3 +12,18 @@ CREATE TABLE species (id INT GENERATED ALWAYS AS IDENTITY, name VARCHAR(45) NOT 
  ALTER TABLE animals DROP COLUMN species;
   ALTER TABLE animals ADD COLUMN species_id INT, ADD CONSTRAINT fk_species_id FOREIGN KEY(species_id) REFERENCES species(id);
    ALTER TABLE animals ADD COLUMN owner_id INT, ADD CONSTRAINT fk_owner_id FOREIGN KEY(owner_id) REFERENCES owners(id);
+
+--    JOIN and RELATIONSHIPS
+CREATE TABLE vets (ID int GENERATED ALWAYS AS INDENITY, name VARCHAR NOT NULL, age INT NOT NULL, date_of_graduation date NOT NULL, PRIMARY KEY(id) );
+-- Created table called specializations with many to many relationship with vets and species -- 
+CREATE TABLE specializations (
+vets_id BIGINT REFERENCES vets (id),
+species_id BIGINT REFERENCES species (id)
+);
+
+-- Created table called visits with many to many relationship with animals, vets and date of visits--
+CREATE TABLE visits (
+vets_id BIGINT REFERENCES vets (id),
+animals_id BIGINT REFERENCES animals (id),
+date_of_visit DATE NOT NULL
+);
