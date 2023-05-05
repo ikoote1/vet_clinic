e3667b400c1add95b979e0d18ab94fdfaab9a56e
ikoote1 <ikootepreim@gmail.com>
2023-05-05 10:05:49 +0300

    adding queries

--- schema.sql
+++ schema.sql
@@ -16,6 +16,7 @@ CREATE TABLE species (id INT GENERATED ALWAYS AS IDENTITY, name VARCHAR(45) NOT 
 --    JOIN and RELATIONSHIPS
 CREATE TABLE vets (ID int GENERATED ALWAYS AS INDENITY, name VARCHAR NOT NULL, age INT NOT NULL, date_of_graduation date NOT NULL, PRIMARY KEY(id) );
 CREATE TABLE specializations (vets_id BIGINT REFERENCES vets(id), species_id BIGINT REFERENCES species(id));
+CREATE TABLE visits(animals_id BIGINT REFERENCES animals(id), vets_id BIGINT REFERENCES vets(id), date_of_visit DATE NOT NULL);
 -- Created table called specializations with many to many relationship with vets and species -- 
 CREATE TABLE specializations (
 vets_id BIGINT REFERENCES vets (id),
