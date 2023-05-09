9439c43d6cef3d9f278bc22658b4024ec86fc0de
ikoote1 <ikootepreim@gmail.com>
2023-05-09 12:55:08 +0300

    challenge

--- data.sql
+++ data.sql
@@ -22,3 +22,24 @@ INSERT INTO visits(animals_id, vets_id, date_of_visit) VALUES
 (1,2,'2021/04/07'),(4,3,'2019/09/29'),(2,4,'2020/10/03'),(2,4,'2020/11/04'),(8,2,'2019/01/24'),
 (8,2,'2019/05/15'),(8,2,'2020/02/27'),(8,2,'2020/08/03'),(10,3,'2020/05/24'),(10, 1,'2021/01/11');
 
+
+-- practice
+CREATE TABLE Projects( ID INT PRIMARY KEY IDENTITY,Name VARCHAR(100),Value DECIMAL(5,2),StartDate DATE,EndDate DATE)
+
+CREATE TABLE Employees(ID INT PRIMARY KEY IDENTITY,FirstName VARCHAR(50),LastName VARCHAR(50),HourlyWage DECIMAL(5,2),HireDate DATE)
+CREATE TABLE ProjectEmployees(ID INT PRIMARY KEY IDENTITY,ProjectID INT,EmployeeID INT,Hours DECIMAL(5,2),CONSTRAINT FK_ProjectEmployees_Projects FOREIGN KEY(ProjectID) REFERENCES  Projects(ID),CONSTRAINT FK_ProjectEmployees_Employees FOREIGN KEY(EmployeeID) REFERENCES  Employees(ID))
+
+CREATE TABLE JobOrders(ID INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,EmployeeID INT,ProjectID INT,Description TEXT,OrderDateTime DATETIME,Quantity INT,Price DECIMAL(5,2),CONSTRAINT FK_JobOrders_Projects FOREIGN KEY(ProjectID) REFERENCES Projects(ID),CONSTRAINT F_JobOrders_Employees FOREIGN KEY(EmployeeID) REFERENCES Employees(ID))
+CREATE TABLE Customers (Name VARCHAR(100),Industry VARCHAR(100),Project1_ID INT,Project1_Feedback TEXT,Project2_ID INT,Project2_Feedback TEXT,ContactPersonID INT,ContactPersonAndRole VARCHAR(255),PhoneNumber VARCHAR(12),Address VARCHAR(255),City VARCHAR(255),Zip VARCHAR(5))
+ALTER TABLE Customers DROP COLUMN Project1_ID, COLUMN Project1_Feedback;
+ALTER TABLE Customers DROP COLUMN Project2_ID
+ALTER TABLE Customers DROP COLUMN project2_feedback
+
+CREATE TABLE ProjectFeedback(ID INT PRIMARY KEY IDENTITY,ProjectID INT,CustomerID INT,Feedback TEXT,CONSTRAINT FK_ProjectFeedbacks_Projects FOREIGN KEY (ProjectID) REFERENCES  Projects(ID),CONSTRAINT FK_ProjectFeedbacks_Customers FOREIGN KEY (CustomerID) REFERENCES  Customers(ID))
+
+-- normalization
+INSERT INTO visits (animals_id, vets_id, date_of_visit) SELECT * FROM (SELECT id FROM animals) animal_ids, (SELECT id FROM vets) vets_ids, generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
+
+insert into owners (name, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
+
+
